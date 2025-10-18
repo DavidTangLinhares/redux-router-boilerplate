83a5158258c32b73f8b22b8d6574f813171b6fed
David Tang <david_tang@web.de>
2025-10-18 15:10:56 +0200

    SetUp Updated

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,20 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { createHistory as history } from 'history';
+import { Router, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import '../assets/stylesheets/application.scss';
 
+// Minimal placeholder reducer so the store has a valid reducer to start with.
+const placeholder = (state = {}) => state;
+
 const reducers = combineReducers({
-  // key: reducer
+  app: placeholder
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
+const history = createBrowserHistory();
 
 // render an instance of the component in the DOM
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,65 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.[contenthash:8].js',
+    publicPath: '/',
+    clean: true
   },
-  devtool: 'sourcemap',
-  devServer: { historyApiFallback: true },
+  devtool: 'source-map',
+  // Silence known deprecation noisy warning from Dart Sass about the legacy JS API
+  // See: https://sass-lang.com/d/legacy-js-api
+  ignoreWarnings: [
+    {
+      message: /legacy JS API is deprecated/
+    }
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    hot: true,
+    port: 3000,
+    open: true
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        use: {
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.s?css$/,
         exclude: /node_modules/,
-        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                quietDeps: true
+              }
+            }
+          }
+        ]
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      inject: 'body'
+    })
+  ],
   resolve: {
     extensions: [ '.js', '.jsx' ]
   }
